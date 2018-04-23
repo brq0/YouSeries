2c9fdac6eb90a6e86427ca8601f3a9cce822e6f7
Konrad Małocha <konrad_malocha@hotmail.com>
2018-04-23 13:43:40 +0200

    Add navigation_bar component

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ const TMDB_API_KEY = 'f32b6b18b2054226bbfb00dfeda586c7';
 let Query = "https://api.themoviedb.org/3/genre/tv/list?api_key="+TMDB_API_KEY;
 
 const App = () => {
-  return (<div>
-          <SearchBar />
-          <div>{Query}</div>
-        </div>)
+  return (
+    <div>
+      <SearchBar />
+      <div>
+        {Query}
+      </div>
+      <GenresMenu />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<GenresMenu />, document.getElementById('genres_menu'));
