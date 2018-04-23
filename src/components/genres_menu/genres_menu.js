adb27e08a9edb2c134c0ed1289c74c94311a8ca5
Shiket <shiket95@gmail.com>
2018-04-23 19:20:04 +0200

    files

--- src/components/genres_menu/genres_menu.js
+++ src/components/genres_menu/genres_menu.js
@@ -3,14 +3,44 @@ import React, { Component } from 'react';
 const API_KEY = 'f32b6b18b2054226bbfb00dfeda586c7'
 const API_URL = 'https://api.themoviedb.org/3/search/tv'
 
+const MovieDb = require('moviedb-promise')
+const moviedb = new MovieDb(API_KEY);
+
+let genres = [];
+
 class GenresMenu extends Component {
 
-  render() {
-    return (
-      <div>
-        menu
-      </div>
-    )
+  constructor(props){
+    super(props);
+
+    this.state = {
+      flag: false
+    }
+
+    moviedb.genreTvList().then(res => {
+      res['genres'].forEach(e=>{
+        genres.push(
+          {
+            name: e['name'],
+            id: e['id']
+          }
+        )
+      })
+      this.state.flag = true;
+      console.log(genres)
+    })
+
   }
+
+  render() {
+    // if(this.state.flag == true){
+      return (
+        <div>
+          {genres}
+          {this.sate}
+        </div>
+      )
+    }
+  // }
 }
 export default GenresMenu;
