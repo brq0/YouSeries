6bd6eb84d6b316a3a89a892ba324a00f43e64b23
Konrad Małocha <konrad_malocha@hotmail.com>
2018-04-23 15:04:30 +0200

    Move search bar to navigation bar

--- src/components/navigation_bar/navigation_bar.js
+++ src/components/navigation_bar/navigation_bar.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './navigation_bar.css';
+import SearchBar from '../search_bar/search_bar';
+
+const TMDB_API_KEY = 'f32b6b18b2054226bbfb00dfeda586c7';
+let Query = "https://api.themoviedb.org/3/genre/tv/list?api_key="+TMDB_API_KEY;
+
 class NavigationBar extends Component{
 
   render(){
@@ -9,7 +14,9 @@ class NavigationBar extends Component{
         <ul className="header">
           <li><a href="#">Stronga Główna</a></li>
           <li><a href="#">Twój Profil</a></li>
+          <li><a><SearchBar /></a></li>
           <li><a href="#">Wyloguj</a></li>
+
         </ul>
       </div>
     );
