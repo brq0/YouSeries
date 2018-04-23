020f716e6ccad0916f4de580904519059b326f18
Konrad Małocha <konrad_malocha@hotmail.com>
2018-04-23 22:22:13 +0200

    Add content new components

---
+++ src/components/content/main_content.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MainContent extends Component{
+
+  render(){
+    return(
+      <div>
+        <a> LISTA FILMÓW </a>
+      </div>
+    );
+  }
+}
+
+export default MainContent;
---
+++ src/components/content/profile_content.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileContent extends Component{
+
+  render(){
+    return(
+      <div>
+        <a>Dane użytkownika</a>
+      </div>
+    );
+  }
+}
+
+export default ProfileContent;
