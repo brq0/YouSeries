02585f796f7f0bdcf2ce8d92dc410bd4c6a28a29
brq0 <bartoszturek0@gmail.com>
2018-04-23 21:29:04 +0200

    genres added

--- src/components/genres_menu/genres_menu.js
+++ src/components/genres_menu/genres_menu.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './genres_menu.css';
 
 const API_KEY = 'f32b6b18b2054226bbfb00dfeda586c7'
-const API_URL = 'https://api.themoviedb.org/3/search/tv'
+const API_URL = 'https://api.themoviedb.org/3/genre/tv/list'
+const QUERY = API_URL+'?api_key='+API_KEY;
 
 // const MovieDb = require('moviedb-promise')
 // const moviedb = new MovieDb(API_KEY);
 
 let genres = [];
 
+window.onload = function() {
+      console.log('load')
+}
+
+function onGenreClick(id){
+  alert(id);
+}
+
 class GenresMenu extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      flag: false
-    }
+      genres: [],
+    };
+  }
 
-    // moviedb.genreTvList().then(res => {
-    //   res['genres'].forEach(e=>{
-    //     genres.push(
-    //       {
-    //         name: e['name'],
-    //         id: e['id']
-    //       }
-    //     )
-    //   })
-    //   this.state.flag = true;
-    //   console.log(genres)
-    // })
-
+  componentDidMount() {
+    axios.get(`${QUERY}`)
+      .then(({ data }) => {
+		  console.log(data['genres']);
+        this.setState({
+          genres: data['genres']
+        })
+      })
   }
 
   render() {
-    // if(this.state.flag == true){
-      return (
-        <div>
-          {genres}
-          // {this.sate}
-        </div>
-      )
-    }
-  // }
+    const { genres } = this.state;
+    return (
+      <div className="genresContainer">
+        {genres.map(genre =>
+          <div key={genre.id} className="genre">
+            <p onClick={()=>onGenreClick(genre.id)}>{genre.name}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
 }
 export default GenresMenu;
