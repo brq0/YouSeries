a5264b7c1358c00ec469b1339519538c1b822c43
Konrad Małocha <konrad_malocha@hotmail.com>
2018-05-02 13:32:35 +0200

    Add logon component and required modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import GenresMenu from './components/genres_menu/genres_menu';
 import NavigationBar from './components/navigation_bar/navigation_bar';
+import Logon from './components/logon/Logon'
+
 import './clear.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 const TMDB_API_KEY = 'f32b6b18b2054226bbfb00dfeda586c7';
 let Query = "https://api.themoviedb.org/3/genre/tv/list?api_key="+TMDB_API_KEY;
@@ -11,8 +17,10 @@ let Query = "https://api.themoviedb.org/3/genre/tv/list?api_key="+TMDB_API_KEY;
 const App = () => {
   return (
     <div>
-      <NavigationBar />
-      <GenresMenu />
+      <div className="text-center">
+        <Logon />
+      </div>
+
     </div>
   );
 }
