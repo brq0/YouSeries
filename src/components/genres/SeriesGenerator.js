2ec5b54affc94471b1ec8a160ddc8a3263c1c630
brq0 <bartoszturek0@gmail.com>
2018-05-02 10:20:57 +0200

    genres img change

--- src/components/genres/SeriesGenerator.js
+++ src/components/genres/SeriesGenerator.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+
+// <div key={r.id} style={{height:'80px', float:'left', marginRight:'5px'}}>
+//   <img src={`http://image.tmdb.org/t/p/w185/${r.poster_path}`} alt=""
+//     style={{width:'50px', height:'80px'}}/>
+// </div>
+
 
 const SeriesGenerator = (props) => {
   console.log(props)
-  const options = props.results.map(r => (
+  let options = props.results.map(r => (
   // onMOUSE DOWN ZAMIAST ONCLICK w suggestion nie wiem dlaczego
   r.poster_path !== null ?
-    <div key={r.id} style={{height:'80px', float:'left', marginRight:'5px'}}>
-      <img src={`http://image.tmdb.org/t/p/w185/${r.poster_path}`} alt=""
-        style={{width:'50px', height:'80px'}}/>
+  <li className='list-group-item' style={{float:'left'}} key={r.id}>
+    <div className='video-list media'>
+    <div className='media-left' style={{float:'left'}}>
+      <img className='media-object' src={`http://image.tmdb.org/t/p/w185/${r.poster_path}`} alt="" style={{width:'75%'}}/>
 	  </div>
-  : <span key={r.id}></span>
+    </div>
+  </li>
+  : <span>elo</span>
 
   ))
+
+
+
+
   return <div style={{ overflow: 'hidden',
      whiteSpace: 'nowrap'}}>
-          <div>{options}</div>
+          <div>{options.filter(e=> e !== null)}</div>
         </div>
 }
 
