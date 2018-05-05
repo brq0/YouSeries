5b4c73983910a06cf58d5b2445b0c14cbcee0469
brq0 <bartoszturek0@gmail.com>
2018-05-05 19:05:20 +0200

    added country in series details

--- src/components/series/SeriesItemDetails.js
+++ src/components/series/SeriesItemDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './series_items_style.css';
+import ReactCountryFlag from 'react-country-flag';
 
 
 function onSeriesClick(id, props){
@@ -8,11 +9,19 @@ function onSeriesClick(id, props){
 }
 
 const SeriesItemDetails = (props) =>{
+  let countries = require("i18n-iso-countries");
+  countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
+
   var show = props.pickedShow;
   var similarSeries = props.similarSeries !== null ? props.similarSeries.results : null;
 
   var year = show.first_air_date !== null ? show.first_air_date.substring(0,4) : "";
-  var genres = show.genres.map(e=><p key={e.id}>{e.name}</p>)
+  var genres = show.genres.map(e=><p key={e.id}>{e.name}</p>);
+  var productionCountries = show.origin_country.map(e=>
+                            <div key={e}>
+                              <ReactCountryFlag style={{display:'inline'}} code={e} svg/>
+                              <p style={{display:'inline', marginLeft:'5px', marginRight:'5px'}}>{countries.getName(e, "en")}</p>
+                            </div>);
 
   const seriesDetails = (<div>
                               <div id="seriesName">{show['name']}</div>
@@ -21,20 +30,28 @@ const SeriesItemDetails = (props) =>{
                               <div id="seasonsAmount">Seasons: {show['seasons'].length}</div>
                               <img src={`http://image.tmdb.org/t/p/w185/${show['poster_path']}`} alt="" />
                               <div id="seriesGenres">Genres: {genres}</div>
+                              <div id="seriesProductionCountries">Country: {productionCountries} </div>
                               <div id="voteCount">Number of votes: {show['vote_count']}</div>
                               <div id="voteAverage">Average vote: {show['vote_average']}</div>
                           </div>)
+
   if(similarSeries !== null && similarSeries['0'] !== undefined){
+    // w tym divie similarSeries slider dla wszystkich z vara similarseries wyswietlane jakos po 4/5 czy cos
     return <div className="seriesItemDetails">
             {seriesDetails}
 
             <br/>
             <p id="similarSeriesLabel">Similar series:</p>
             <div id="similarSeries">
+
+
               <img src={`http://image.tmdb.org/t/p/w185/${similarSeries['0'].poster_path}`}
                     style={{cursor:'pointer'}}
                     alt="" onClick={()=>onSeriesClick(similarSeries['0'].id, props)}/>
               </div>
+
+
+
            </div>
   }else{
     return <div className="seriesItemDetails">
